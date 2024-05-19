a3a5de7f302f28a7caa201d5eb1af21203d932db
Alex Meyer <alex.meyer.cz@gmail.com>
2024-05-19 16:45:12 +0200

    56. LinksDropdown Component

--- src/components/navbar/LinksDropdown.tsx
+++ src/components/navbar/LinksDropdown.tsx
@@ -1,12 +1,51 @@
 import React, { type FC } from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from '@/components/ui/dropdown-menu'
+import { LuAlignLeft } from 'react-icons/lu'
+import Link from 'next/link'
+import { Button } from '../ui/button'
+import UserIcon from './UserIcon'
+import { links } from '@/utils/links'
+import SignOutLink from './SignOutLink'
 
 const f = '⇒ LinksDropdown.tsx (LinksDropdown):'
 
 const LinksDropdown: FC = () => {
   return (
-    <div>
-      <h1>LinksDropdown</h1>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant='outline'
+          className='flex max-w-[100px] gap-4'
+        >
+          <LuAlignLeft className='h-6 w-6' />
+          <UserIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className='w-52'
+        align='start'
+        sideOffset={10}
+      >
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link
+                href={link.href}
+                className='w-full capitalize'
+              >
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          )
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 export default LinksDropdown
--- src/components/navbar/UserIcon.tsx
+++ src/components/navbar/UserIcon.tsx
@@ -1,12 +1,9 @@
 import React, { type FC } from 'react'
+import { LuUser2 } from 'react-icons/lu'
 
 const f = '⇒ UserIcon.tsx (UserIcon):'
 
 const UserIcon: FC = () => {
-  return (
-    <div>
-      <h1>UserIcon</h1>
-    </div>
-  )
+  return <LuUser2 className='h-6 w-6 rounded-full bg-primary text-white' />
 }
 export default UserIcon
