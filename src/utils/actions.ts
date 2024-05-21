e871272dd4b11fbff19c0107293003e3ea9f81e2
Alex Meyer <alex.meyer.cz@gmail.com>
2024-05-21 20:29:20 +0200

    70. Create Profile Model and createProfileAction

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,6 +1,10 @@
 'use server'
 
-import { profileSchema } from './schemas'
+import { profileSchema } from '@/utils/schemas'
+import db from '@/utils/db'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server'
 
 const f = '⇒ actions.ts:'
 
@@ -9,12 +13,29 @@ export const createProfileAction = async (
   formData: FormData,
 ) => {
   try {
+    const user = await currentUser()
+    if (!user) throw new Error('user not found')
+
+    console.log(f, 'user →', user)
     const rawData = Object.fromEntries(formData)
     const validatedFields = profileSchema.parse(rawData)
-    console.log(f, 'validatedFields →', validatedFields)
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validatedFields,
+      },
+    })
+    // Update user metadata
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: { hasProfile: true },
+    })
     return { message: 'profile created' }
   } catch (error) {
     console.log(f, 'error →', error)
-    return { message: 'there was an error' }
+    return {
+      message: error instanceof Error ? error.message : 'An error occurred',
+    }
   }
 }
