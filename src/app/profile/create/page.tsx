1867345e98a482ff95c305fd10c7ea861490f72b
Alex Meyer <alex.meyer.cz@gmail.com>
2024-05-22 20:04:53 +0200

    72. Modify Create Profile Page

--- src/app/profile/create/page.tsx
+++ src/app/profile/create/page.tsx
@@ -4,10 +4,16 @@ import FormInput from '@/components/form/FormInput'
 import { SubmitButton } from '@/components/form/Buttons'
 import FormContainer from '@/components/form/FormContainer'
 import { createProfileAction } from '@/utils/actions'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 
 const f = '⇒ page.tsx (CreateProfilePage):'
 
-const CreateProfilePage: FC = () => {
+const CreateProfilePage: FC = async () => {
+  const user = await currentUser()
+  if (user?.privateMetadata.hasProfile) {
+    redirect('/profile')
+  }
   return (
     <div>
       <h1 className='mb-8 text-2xl font-semibold capitalize'>new user</h1>
